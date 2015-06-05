518e546840c7be40923ac610cebaaf4810271f82
Hoobie <techno-3542>
2015-06-05 18:13:22 +0200

    Add basic mirrors solution

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include "cell_type.h"
 #include "Cell.h"
 
-void addRays(vector<vector<Cell>> board, int width, int height, Cell &laser);
+void addRays(vector<vector<Cell>> board, int width, int height, Cell &device);
 pair<short, short> getRaySteps(unsigned short direction);
 unsigned short laserToPipeDirection(unsigned short laserDirection);
 bool solve(vector<vector<Cell>> board, int width, int height, vector<Cell> mirrors);
 void printBoard(vector<vector<Cell>> board, int width, int height);
 void prepareBoardCopy(vector<vector<Cell>> &boardCopy, vector<Cell> mirrorsCopy, unsigned int x, unsigned int y,
                       unsigned short mirrorDirection);
+
+unsigned short getReflectionDirection(cell_type mirror_type, unsigned short mirrorDirection,
+                                      unsigned short rayDirection);
 
 using namespace std;
 
@@ -174,34 +178,104 @@ bool solve(vector<vector<Cell>> board, int width, int height, vector<Cell> mirro
 
 void prepareBoardCopy(vector<vector<Cell>> &boardCopy, vector<Cell> mirrorsCopy, unsigned int y, unsigned int x,
                       unsigned short mirrorDirection) {
+    color_type color = boardCopy[x][y].getColor();
     Cell m = mirrorsCopy.back();
     mirrorsCopy.pop_back();
     m.setDirection(mirrorDirection);
     m.setX(x);
     m.setY(y);
     boardCopy[x][y] = m;
-    // TODO: change rays
+    // TODO: remove wrong and add new rays
 }
 
-void addRays(vector<vector<Cell>> board, int width, int height, Cell &laser) {
-    pair<short, short> steps = getRaySteps(laser.getDirection());
-    for (pair<unsigned int, unsigned int> i(laser.getX() + steps.first, laser.getY() + steps.second);
+void addRays(vector<vector<Cell>> board, int width, int height, Cell &device) {
+    pair<short, short> steps = getRaySteps(device.getDirection());
+
+    if (steps.first == 0 && steps.second == 0) {
+        return;
+    }
+
+    for (pair<unsigned int, unsigned int> i(device.getX() + steps.first, device.getY() + steps.second);
          i.first > 0 && i.first < width && i.second > 0 && i.second < height; i.first += steps.first, i.second += steps.second) {
         Cell &cell = board[i.first][i.second];
         if (isBlock(cell.getCellType())) {
             break;
         }
-        ray_type ray = { laser.getDirection(), laser.getColor() };
+        const ray_type &ray = { device.getDirection(), device.getColor() };
+        vector<ray_type> rays = cell.getRays();
+        if (find(rays.begin(), rays.end(), ray) != rays.end()) {
+            // if found exactly the same ray then break
+            break;
+        }
         if (isPipe(cell.getCellType())) {
-            if (cell.getDirection() != laserToPipeDirection(laser.getDirection())) {
+            if (cell.getDirection() != laserToPipeDirection(device.getDirection())) {
+                break;
+            }
+        }
+        if (isMirror(cell.getCellType())) {
+            // create pseudo-laser with proper reflection direction
+            unsigned short direction = getReflectionDirection((BL), cell.getDirection(), device.getDirection());
+            Cell pseudoLaser = Cell(NONE, i.first, i.second, direction, device.getColor());
+            addRays(board, width, height, pseudoLaser);
+            // if not splitter then only one ray
+            if (cell.getCellType() != LP) {
                 break;
             }
         }
         cell.setX(i.first);
         cell.setY(i.second);
-        cell.setDirection(laser.getDirection());
-        cell.setColor(laser.getColor());
+        cell.setDirection(device.getDirection());
+        cell.setColor(device.getColor());
         cell.addRay(ray);
+    }
+}
+
+unsigned short getReflectionDirection(cell_type mirror_type, unsigned short mirrorDirection,
+                                      unsigned short rayDirection) {
+    switch (mirror_type) {
+        case LU:
+        case LP:
+            switch (mirrorDirection) {
+                case 0:
+                    if (rayDirection == 3) return 1;
+                    if (rayDirection == 5) return 7;
+                    return 8;
+                case 1:
+                    if (rayDirection == 4) return 2;
+                    if (rayDirection == 6) return 0;
+                    return 8;
+                case 2:
+                    if (rayDirection == 5) return 3;
+                    if (rayDirection == 7) return 1;
+                    return 8;
+                case 3:
+                    if (rayDirection == 0) return 2;
+                    if (rayDirection == 6) return 4;
+                    return 8;
+                case 4:
+                    if (rayDirection == 1) return 3;
+                    if (rayDirection == 7) return 5;
+                    return 8;
+                case 5:
+                    if (rayDirection == 0) return 6;
+                    if (rayDirection == 2) return 5;
+                    return 8;
+                case 6:
+                    if (rayDirection == 1) return 7;
+                    if (rayDirection == 3) return 5;
+                    return 8;
+                case 7:
+                    if (rayDirection == 2) return 0;
+                    if (rayDirection == 4) return 6;
+                    return 8;
+                default:
+                    throw 3;
+            }
+        case LK:
+            throw "Not implemented yet!!!";
+            break;
+        default:
+            throw 2;
     }
 }
 
